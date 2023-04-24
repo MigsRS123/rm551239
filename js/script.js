101ea46477b1b4658d80eb7bce1c3b4df28702fd
MigsRS123 <rm551239@fiap.com.br>
2023-04-24 09:53:08 -0300

    layout cores

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
-//Recuperando o botão com o getElementById
-const botão = document.getElementById('btn-1');
+// //Recuperando o botão com o getElementById
+// const botão = document.getElementById('btn-1');
 
 // Recuperando um item expecifico com a função
 //querySelector(parâmetro)
@@ -9,17 +9,17 @@ const botão = document.getElementById('btn-1');
 
 // Recuperar uma lista de emlemtos com a função
 // querySelectorAll 
-const seletores = document.querySelectorAll("li");
+// const seletores = document.querySelectorAll("li");
 
-seletores.forEach( (Item)=>{
+// seletores.forEach( (Item)=>{
 
-    if (Item.textContent == "Item-15") {
-        let meuItem = Item;
-        console.log('Items selecionados : ${Item.textContent}');
-        meuItem.textContent = "Tá dominado"
-    }
+//     if (Item.textContent == "Item-15") {
+//         let meuItem = Item;
+//         console.log('Items selecionados : ${Item.textContent}');
+//         meuItem.textContent = "Tá dominado"
+//     }
 
-})
+// })
 
 //setTimeOut
 //setInterval
@@ -28,3 +28,61 @@ seletores.forEach( (Item)=>{
 //Math.Random
 //Math.Floor
 
+let tmp = "";
+
+function mudarCor() {
+
+    let r = 0;
+    let g = 0;
+    let b = 0;
+
+    r = Math.round(Math.random()*255);
+    g = Math.round(Math.random()*255);
+    b = Math.round(Math.random()*255);
+    
+    const cabecalho = document.querySelector('.cabecalho');
+    cabecalho.style.backgroundColor = `rgb(${r},${g},${b})`;
+
+    tmp = setTimeout(mudarCor, 3000);
+}
+
+mudarCor();
+
+
+function mudarCor1() {
+
+    let r = 0;
+    let g = 0;
+    let b = 0;
+
+    r = Math.round(Math.random()*255);
+    g = Math.round(Math.random()*255);
+    b = Math.round(Math.random()*255);
+    
+    const cabecalho = document.querySelector('.banner-1');
+    cabecalho.style.backgroundColor = `rgb(${r},${g},${b})`;
+
+    tmp = setTimeout(mudarCor1, 3000);
+}
+
+mudarCor1();
+
+
+
+function mudarCor2() {
+
+    let r = 0;
+    let g = 0;
+    let b = 0;
+
+    r = Math.round(Math.random()*255);
+    g = Math.round(Math.random()*255);
+    b = Math.round(Math.random()*255);
+    
+    const cabecalho = document.querySelector('.banner-2');
+    cabecalho.style.backgroundColor = `rgb(${r},${g},${b})`;
+
+    tmp = setTimeout(mudarCor2, 3000);
+}
+
+mudarCor2();
