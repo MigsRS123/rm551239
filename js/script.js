8c80d54f38c819e3fcc9d1e59fdfddf7899703f2
MigsRS123 <rm551239@fiap.com.br>
2023-05-08 09:47:40 -0300

    Algo

--- js/script.js
+++ js/script.js
@@ -28,61 +28,112 @@
 //Math.Random
 //Math.Floor
 
-let tmp = "";
+// let tmp = "";
 
-function mudarCor() {
+// function mudarCor() {
 
-    let r = 0;
-    let g = 0;
-    let b = 0;
+//     let r = 0;
+//     let g = 0;
+//     let b = 0;
 
-    r = Math.round(Math.random()*255);
-    g = Math.round(Math.random()*255);
-    b = Math.round(Math.random()*255);
+//     r = Math.round(Math.random()*255);
+//     g = Math.round(Math.random()*255);
+//     b = Math.round(Math.random()*255);
     
-    const cabecalho = document.querySelector('.cabecalho');
-    cabecalho.style.backgroundColor = `rgb(${r},${g},${b})`;
+//     const cabecalho = document.querySelector('.cabecalho');
+//     cabecalho.style.backgroundColor = `rgb(${r},${g},${b})`;
 
-    tmp = setTimeout(mudarCor, 3000);
-}
+//     tmp = setTimeout(mudarCor, 3000);
+// }
 
-mudarCor();
+// mudarCor();
 
 
-function mudarCor1() {
+// function mudarCor1() {
 
-    let r = 0;
-    let g = 0;
-    let b = 0;
+//     let r = 0;
+//     let g = 0;
+//     let b = 0;
 
-    r = Math.round(Math.random()*255);
-    g = Math.round(Math.random()*255);
-    b = Math.round(Math.random()*255);
+//     r = Math.round(Math.random()*255);
+//     g = Math.round(Math.random()*255);
+//     b = Math.round(Math.random()*255);
     
-    const cabecalho = document.querySelector('.banner-1');
-    cabecalho.style.backgroundColor = `rgb(${r},${g},${b})`;
+//     const cabecalho = document.querySelector('.banner-1');
+//     cabecalho.style.backgroundColor = `rgb(${r},${g},${b})`;
 
-    tmp = setTimeout(mudarCor1, 3000);
-}
+//     tmp = setTimeout(mudarCor1, 3000);
+// }
 
-mudarCor1();
+// mudarCor1();
 
 
 
-function mudarCor2() {
+// function mudarCor2() {
 
-    let r = 0;
-    let g = 0;
-    let b = 0;
+//     let r = 0;
+//     let g = 0;
+//     let b = 0;
 
-    r = Math.round(Math.random()*255);
-    g = Math.round(Math.random()*255);
-    b = Math.round(Math.random()*255);
+//     r = Math.round(Math.random()*255);
+//     g = Math.round(Math.random()*255);
+//     b = Math.round(Math.random()*255);
     
-    const cabecalho = document.querySelector('.banner-2');
-    cabecalho.style.backgroundColor = `rgb(${r},${g},${b})`;
+//     const cabecalho = document.querySelector('.banner-2');
+//     cabecalho.style.backgroundColor = `rgb(${r},${g},${b})`;
 
-    tmp = setTimeout(mudarCor2, 3000);
-}
+//     tmp = setTimeout(mudarCor2, 3000);
+// }
 
-mudarCor2();
+// mudarCor2();
+
+
+// function mudaBanner() {
+//     const imgElemnt1 = document.querySelector('.banner-1 img')
+//     // console.log(imgElemnt1);
+//     let nr = Math.cell(Math.random()*3);
+//     imgElemnt1.src = './img/banner-lateral-'+(nr)+'.png';
+//     setTimeout(mudaBanner, 2000)
+// }
+// function mudaBanner2() {
+//     const imgElemnt1 = document.querySelector('.banner-2 img')
+//     // console.log(imgElemnt1);
+//     let nr = Math.cell(Math.random()*3);
+//     imgElemnt1.src = './img/banner-lateral-'+(nr)+'.png';
+//     setTimeout(mudaBanner2, 2000)
+// }
+// mudaBanner();
+
+// function mudaBanner() {
+//     const imgElemnt1 = document.querySelector('.banner-1 img');
+//     // console.log(imgElemnt1);
+//     let nr = Math.ceil (Math.random()*3);
+//     imgElemnt1.src = './img/banner-lateral-'+(nr)+'.png';
+//     setTimeout(mudaBanner, 2000);
+
+//     const imgElemnt2 = document.querySelector('.banner-2 img');
+//     // console.log(imgElemnt1);
+//     nr = Math.ceil (Math.random()*3);
+//     imgElemnt2.src = './img/banner-lateral-'+(nr)+'.png';
+//     setTimeout(mudaBanner, 2000);
+// }
+// mudaBanner();
+
+const botao = document.querySelector("button");
+// console.log(botao);
+
+botao.addEventListener("click", function(){
+    if(this.textContent == "Ligar"){
+        const imgElemnt = document.querySelector("img[alt='lampadaoff']")
+        imgElemnt.src = "../img/pic_bulbon.gif"
+        imgElemnt.alt = "lampadaon"
+        this.textContent = "Desligar"
+    
+    }else{
+        const imgElemnt = document.querySelector("img[alt='lampadaon']")
+        imgElemnt.src = "../img/pic_bulboff.gif"
+        imgElemnt.alt = "lampadaof"
+        this.textContent = "Ligar"
+    }
+});
+
